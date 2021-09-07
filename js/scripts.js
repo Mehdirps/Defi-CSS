fe0b4aa57ecaf35854d5bab69bfcc04bb1e7539d
Mehdirps <Mehdi.raposo77@gmail.com>
2021-09-07 10:18:47 +0200

    random a la con

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ let boule = document.querySelector(".noir");
 let texte = document.querySelector("h1")
 let ombre = document.querySelector(".divombre")
 let blancboule = document.querySelector(".blanc")
+let body = document.querySelector("body")
 let phrases = [
     "Essaye plus tard",
     "Essaye encore",
@@ -32,18 +33,33 @@ boule.addEventListener("click", function (event) {
             event.preventDefault();
         }
     }
-    let nombre = Math.floor(Math.random() * phrases.length);
-    texte.textContent = phrases[nombre];
-    // texte.style.fontSize = "1.5rem"
-    texte.style.fontSize= Math.floor(Math.random() * 5) + "rem";
-    texte.style.color= '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-    boule.style.borderRadius= Math.floor(Math.random() * 50) + "%";
-    boule.style.background = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-    ombre.style.background= '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-    blancboule.style.background= '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-    color1 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-    color2 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-    color3 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-    boule.style.background= "radial-gradient(farthest-corner at top right," + color1 +"," + color2 + "," + color3 + ")";
-    texte.style.fontFamily = Math.floor(Math.random() + 5) + "";
+    function getChrono() {
+
+        let nombre = Math.floor(Math.random() * phrases.length);
+        texte.textContent = phrases[nombre];
+        texte.style.fontSize = "1.5rem"
+        // texte.style.fontSize= Math.floor(Math.random() * 5) + "rem";
+        texte.style.color= '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        boule.style.borderRadius= Math.floor(Math.random() * 50) + "%";
+        // boule.style.width=Math.floor(Math.random() * 500) + "px";
+        blancboule.style.borderRadius= Math.floor(Math.random() * 50) + "%";
+        boule.style.background = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        ombre.style.background= '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        blancboule.style.background= '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        color1 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        color2 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        color3 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        color4 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        color5 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        color6 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        boule.style.background= "radial-gradient(farthest-corner at top right," + color1 +"," + color2 + "," + color3 + "," + color4 +"," + color5 + "," + color6 + ")";
+        let fontType = ["Arial", "Verdana", "Helvetica"];
+        texte.style.fontFamily = Math.floor(Math.random(),fontType * 5);
+        body.style.background = "radial-gradient(farthest-corner at top right," + color1 +"," + color2 + "," + color3 + "," + color4 +"," + color5 + "," + color6 + ")";
+        body.style.backgroundRepeat = "no-repeat";
+        body.style.backgroundSize= "cover";
+    }
+    let compteurdown = setInterval(() => {
+        getChrono()
+    }, 400);
 })
